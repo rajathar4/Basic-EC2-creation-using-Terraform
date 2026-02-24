a277da27dc7b866da0ec5b506fc9b4d39415aead
rajathar4 <rajathar6@gmail.com>
2026-02-24 23:51:04 +0530

    Basic EC2 creation using Terraform

---
+++ main.tf
@@ -0,0 +1,39 @@
+# Create Security Group
+resource "aws_security_group" "web_sg" {
+  name = "web-sg"
+
+  ingress {
+    description = "Allow SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Create EC2 Instance
+resource "aws_instance" "web" {
+  ami           = "ami-0f58b397bc5c1f2e8"  # Amazon Linux (ap-south-1)
+  instance_type = "t3.micro"
+
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
+
+  tags = {
+    Name = "Terraform-EC2"
+  }
+}
